7a953295013d75bf1fd2e367c49283a688e89965
ike barranco <barranco.ike@outlook.com>
2020-05-15 03:30:07 -0600

    added initial comments to the files

---
+++ employeeDB.sql
@@ -0,0 +1,11 @@
+-- drop db if exists
+
+-- create db 
+
+-- user this db
+
+-- create table 1
+
+-- create table 2
+
+-- create table 3
---
+++ sqlServer.js
@@ -0,0 +1,16 @@
+// Import depenencies
+    //Inquirer
+    //sql
+
+
+// Create Connection to the sql
+
+// Connect SQL to create queries
+
+// CREATE
+
+// READ
+
+// UPDATE
+
+// DELETE
